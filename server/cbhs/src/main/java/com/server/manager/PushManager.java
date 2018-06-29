c57a52cd95882604de77cf38f7a031ef4b9761b6
qmf <651857512@qq.com>
2018-06-29 10:20:39 +0800

    推送修改

--- server/cbhs/src/main/java/com/server/manager/PushManager.java
+++ server/cbhs/src/main/java/com/server/manager/PushManager.java
@@ -28,12 +28,11 @@ public class PushManager {
 	/**
 	 * 推送消息
 	 * 
-	 * @Title: pushMessage @Description: TODO(这里用一句话描述这个方法的作用) @param
-	 *         personOids @param title @param alert @param message @param ext
-	 *         设定文件 @return void 返回类型 @throws
+	 * @Title: pushMessage @Description: TODO(这里用一句话描述这个方法的作用) @param personOids @param title @param alert @param message @param ext 设定文件 @return void 返回类型 @throws
 	 */
 	public void pushExamMessage(List<Integer> personOids, String title, String alert, String message, String ext) {
-		List<String> registrationIds = queryFactory.select(QCbhsAccount.cbhsAccount.ryToken).from(QCbhsAccount.cbhsAccount).where(QCbhsAccount.cbhsAccount.user.oid.in(personOids).and(QCbhsAccount.cbhsAccount.ryToken.isNotNull()).and(QCbhsAccount.cbhsAccount.ryToken.isNotEmpty())).fetch();
+		List<String> registrationIds = queryFactory.select(QCbhsAccount.cbhsAccount.ryToken).from(QCbhsAccount.cbhsAccount)
+				.where(QCbhsAccount.cbhsAccount.user.oid.in(personOids).and(QCbhsAccount.cbhsAccount.ryToken.isNotNull()).and(QCbhsAccount.cbhsAccount.ryToken.isNotEmpty())).fetch();
 		if (registrationIds != null && registrationIds.size() > 0) {
 			JiguangPush jp = new JiguangPush();
 			executor.submit(new Runnable() {
@@ -47,20 +46,24 @@ public class PushManager {
 
 	public void pushUpgradeMessage(CbhsVersion version) {
 		JiguangPush jp = new JiguangPush();
+		List<String> registrationIds = queryFactory.select(QCbhsAccount.cbhsAccount.ryToken).from(QCbhsAccount.cbhsAccount)
+				.where(QCbhsAccount.cbhsAccount.ryToken.isNotNull().and(QCbhsAccount.cbhsAccount.ryToken.isNotEmpty())).fetch();
 		executor.submit(new Runnable() {
 			@Override
 			public void run() {
-				jp.jiguangPush(null, JiguangPush.type_upgrade, version.getPushtitle(), version.getPushAlert(), version.getPushAlert(), "versionInfo", JSON.toJSONString(version));
+				jp.jiguangPush(registrationIds, JiguangPush.type_upgrade, version.getPushtitle(), version.getPushAlert(), version.getPushAlert(), "versionInfo", JSON.toJSONString(version));
 			}
 		});
 	}
 
 	public void pushAllMessage(String title, int type, String alert, String ext) {
 		JiguangPush jp = new JiguangPush();
+		List<String> registrationIds = queryFactory.select(QCbhsAccount.cbhsAccount.ryToken).from(QCbhsAccount.cbhsAccount)
+				.where(QCbhsAccount.cbhsAccount.ryToken.isNotNull().and(QCbhsAccount.cbhsAccount.ryToken.isNotEmpty())).fetch();
 		executor.submit(new Runnable() {
 			@Override
 			public void run() {
-				jp.jiguangPush(null, type, title, alert, alert, "", "");
+				jp.jiguangPush(registrationIds, type, title, alert, alert, "", "");
 			}
 		});
 	}
