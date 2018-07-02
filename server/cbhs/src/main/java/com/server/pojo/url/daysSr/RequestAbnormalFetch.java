dca9ba27aaa1ef75fa829217fc796f169ad93929
zaizhuzhu123 <651857512@qq.com>
2018-07-02 19:39:47 +0800

    请求

---
+++ server/cbhs/src/main/java/com/server/pojo/url/daysSr/RequestAbnormalFetch.java
@@ -0,0 +1,28 @@
+package com.server.pojo.url.daysSr;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.Serializable;
+import io.swagger.annotations.ApiModelProperty;
+import com.server.pojo.bean.*;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+import org.hibernate.validator.constraints.*;
+public class RequestAbnormalFetch implements Serializable {
+	private static final long serialVersionUID = 1L;
+	@ApiModelProperty(value = "工程项目ID")
+	private Integer projectOid= 0;
+
+	public Integer getProjectOid() { 
+
+    	return this.projectOid;
+
+	}
+
+	public void setProjectOid(Integer projectOid) {
+ 
+        this.projectOid = projectOid;
+
+	}
+
+
+}
