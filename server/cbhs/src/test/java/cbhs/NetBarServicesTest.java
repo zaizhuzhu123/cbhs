fce8ad0986fdd0caf97d76ac8a6750a0a22298e4
qmf <651857512@qq.com>
2018-07-02 17:55:26 +0800

    -----------

--- server/cbhs/src/test/java/cbhs/NetBarServicesTest.java
+++ server/cbhs/src/test/java/cbhs/NetBarServicesTest.java
@@ -24,21 +24,21 @@ public class NetBarServicesTest {
 	/**
 	 * 动态调用方式
 	 */
-	@Test
-	public void cl2() {
-		// 创建动态客户端
-		JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
-		Client client = dcf.createClient("http://localhost:8080/services/NetbarServices?wsdl");
-		// 需要密码的情况需要加上用户名和密码
-		// client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME,
-		// PASS_WORD));
-		Object[] objects = new Object[0];
-		try {
-			// invoke("方法名",参数1,参数2,参数3....);
-			objects = client.invoke("sayHello", "Leftso");
-			System.out.println("返回数据:" + objects[0]);
-		} catch (java.lang.Exception e) {
-			e.printStackTrace();
-		}
-	}
+	// @Test
+	// public void cl2() {
+	// // 创建动态客户端
+	// JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+	// Client client = dcf.createClient("http://localhost:8080/services/NetbarServices?wsdl");
+	// // 需要密码的情况需要加上用户名和密码
+	// // client.getOutInterceptors().add(new ClientLoginInterceptor(USER_NAME,
+	// // PASS_WORD));
+	// Object[] objects = new Object[0];
+	// try {
+	// // invoke("方法名",参数1,参数2,参数3....);
+	// objects = client.invoke("sayHello", "Leftso");
+	// System.out.println("返回数据:" + objects[0]);
+	// } catch (java.lang.Exception e) {
+	// e.printStackTrace();
+	// }
+	// }
 }
